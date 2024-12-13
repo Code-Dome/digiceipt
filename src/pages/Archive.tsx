c4d29838946e2c36e0b728a053a7c81458e9846e
BrandonGGL <brandon.schultz@gamesglobal.com>
2024-12-13 09:51:53 +0200

    Update Archive.tsx

--- src/pages/Archive.tsx
+++ src/pages/Archive.tsx
@@ -1,4 +1,4 @@
-limport { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { Receipt } from "@/types/receipt";
 import { InvoiceTable } from "@/components/InvoiceList/InvoiceTable";
 import { Button } from "@/components/ui/button";
